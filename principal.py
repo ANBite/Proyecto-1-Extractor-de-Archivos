e3367886ca656e20f078d1001dc58028e91cdf2f
ANBite <124651559+ANBite@users.noreply.github.com>
2024-10-28 21:29:40 -0600

    Ventana principal

--- principal.py
+++ principal.py
@@ -12,10 +12,32 @@ window.title("Extractor de Archivos GIF") #Se le asigna un nombre a la ventana
 window.resizable(True, True)
 window.grab_set() #Queda debajo de cualquier ventana que se abra
 util_ventana.centrar_ventana(window, 1550, 850)
-fondo = util_imagen.leer_imagen("./imagenes/imgfondo.jpg", (50,40))
-background_label = ctk.CTkLabel(master=window, image=fondo, text="", width=0, height=0)
+fondo = util_imagen.leer_imagen("./imagenes/imgfondo.jpg", (1950,1150))
+background_label = ctk.CTkLabel(master=window, text="",image=fondo, width=0, height=0)
+background_label.place(relx=0, rely=0, relwidth=1, relheight=1)
 
 
+"""| Cajas de texto |"""
+def scrool(event):
+    # Desplazarse verticalmente con la rueda del mouse
+    box1.yview_scroll(int(-1*(event.delta/120)), "units")
 
 
+def zoom(event):
+    global size #Tamaño de las letras
+    if event.delta > 0 :
+        size += 1
+    else:
+        size -= 1
+    box1.configure(font=("Times New Roman", size))
+    
+size = 12 #Tamaño original de las letras
+
+box1 = ctk.CTkTextbox(master=window, wrap=ctk.NONE, font=("Times New Roman", size), width=1512, height=250)
+box1.place(x=10, y=10)
+
+box1.bind("<MouseWheel>", scrool)
+
+
+window.bind("<Control-MouseWheel>", zoom)
 window.mainloop() #Se inicializa la ventana
